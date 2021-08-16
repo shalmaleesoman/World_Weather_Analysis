8283cf33af2dfd2ab64bfef88015206586c7e6a5
Shalmalee Soman <shalmalee.soman@gmail.com>
2021-08-15 20:51:41 -0400

    message

---
+++ config2.py
@@ -0,0 +1,3 @@
+weather_api_key="33dd187f5ba605b188e3bc72d598fbf0"
+# Google API Key
+g_key = "AIzaSyAHO9amURnpT0UvxnjZFEFEXs7vk3BaTWo"
---
+++ config3.py
@@ -0,0 +1,2 @@
+weather_api_key = "07b8063038a00b4adeb480983f0927b5"
+g_key = "AIzaSyBs7bcHd5rAG95ZbUoS3slmNTrniXR78sI"
